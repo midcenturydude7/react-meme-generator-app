4ae0666ddf0f029d28e00c7e1fe09ceba3bf5fd3
midCenturyDude7 <mgriffes@gmail.com>
2023-01-09 11:52:54 -0800

    Added meme text functionality

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -11,7 +11,15 @@ export default function Meme() {
 
   const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
-  // const [memeImg, setMemeImg] = React.useState("http://i.imgflip.com/1bij.jpg");
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      };
+    });
+  }
 
   function getMemeImg() {
     const memesArray = allMemeImages.data.memes;
@@ -29,18 +37,26 @@ export default function Meme() {
         <input 
           type="text" 
           placeholder="Top text"
-          className="meme--form-input"          
+          className="meme--form-input"  
+          name="topText"
+          value={meme.topText} 
+          onChange={handleChange}
         />
         <input 
           type="text"
           placeholder="Bottom text"
           className="meme--form-input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
         <button className="meme--form-btn" onClick={getMemeImg}>
           Get a new meme image 🖼
         </button>
-        <div className="meme--img-container">
+        <div className="meme-base-container">
           <img src={meme.randomImg} alt="Dynamically Generated Meme Image" className="meme--img" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
         </div>
       </div>
     </main>
