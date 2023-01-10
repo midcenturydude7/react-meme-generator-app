cf8ea51d13d7c26815dd1f815880917a9f611c5a
midCenturyDude7 <mgriffes@gmail.com>
2023-01-09 21:25:25 -0800

    Refactored api call with async/await syntax via React.useEffect()

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -10,12 +10,21 @@ export default function Meme() {
 
   const [allMemes, setAllMemes] = React.useState([]);
   
-  React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => setAllMemes(data.data.memes));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then(res => res.json())
+  //     .then(data => setAllMemes(data.data.memes));
+  // }, []);
  
+  React.useEffect(() => {
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    }
+    getMemes();
+  }, []);
+
   function handleChange(event) {
     const {name, value} = event.target;
     setMeme(prevMeme => {
